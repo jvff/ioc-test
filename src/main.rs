06c0bae95e139d71fa258aa6dac3c2a68324d8e6
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-07-25 18:37:44 +0000

    Make test functions private to the module
    - Export only a run_test() function
    - Refactor test functions to set-up a given IocTest instance

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate ioc_test;
 use ioc_test::tests::*;
 
 fn main() {
-    match test_enable_channel_output() {
+    match run_test() {
         Ok(_) => println!("Test Successful"),
         Err(error) => println!("Test FAILED: {}", error),
     }
