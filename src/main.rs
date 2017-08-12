lnls-sirius/agilent33521a-epics-ioc

Create main function to run the test

test/src/main.rs
@@ -1,0 +1,10 @@
+extern crate agilent33521a_ioc_test;
+
+use agilent33521a_ioc_test::tests::*;
+
+fn main() {
+    match test_enable_channel_output() {
+        Ok(_) => println!("Test Successful"),
+        Err(error) => println!("Test FAILED: {}", error),
+    }
+}

jvff/ioc-test

src/main.rs
@@ -1,0 +1,10 @@
+extern crate ioc_test;
+
+use ioc_test::tests::*;
+
+fn main() {
+    match test_enable_channel_output() {
+        Ok(_) => println!("Test Successful"),
+        Err(error) => println!("Test FAILED: {}", error),
+    }
+}