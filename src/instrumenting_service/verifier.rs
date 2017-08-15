b4f07af35e836862893123a91bf9cfea16011ebe
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-15 17:34:02 +0000

    Add Error associated type to Verifier
    - The verifier implementation can return
      - Ok(true) when it has finished verification
      - Ok(false) when it still is verifying
      - Err(_) when verification failed

--- src/instrumenting_service/verifier.rs
+++ src/instrumenting_service/verifier.rs
@@ -1,8 +1,9 @@
 pub trait Verifier {
     type Request;
     type Response;
+    type Error;
 
     fn request(&mut self, request: &Self::Request);
     fn response(&mut self, response: &Self::Response);
-    fn has_finished(&self) -> bool;
+    fn has_finished(&self) -> Result<bool, Self::Error>;
 }
