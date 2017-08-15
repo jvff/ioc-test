20c0c5fd3aedb3a64f9d6eeb0b149b66ea8fa065
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-15 17:59:05 +0000

    Create VerifySequence implementation
    - Verifies first one verifier, then another (after the first completes)

--- src/instrumenting_service/mod.rs
+++ src/instrumenting_service/mod.rs
@@ -3,4 +3,5 @@ mod service_instrumenter;
 
 mod request_verifier;
 mod verifier;
+mod verify_sequence;
 mod verify_two;
---
+++ src/instrumenting_service/verify_sequence.rs
@@ -0,0 +1,53 @@
+use super::verifier::Verifier;
+
+pub struct VerifySequence<A, B>
+where
+    A: Verifier,
+    B: Verifier<Request = A::Request, Response = A::Response, Error = A::Error>,
+{
+    first: A,
+    second: B,
+}
+
+impl<A, B> VerifySequence<A, B>
+where
+    A: Verifier,
+    B: Verifier<Request = A::Request, Response = A::Response, Error = A::Error>,
+{
+    pub fn new(first: A, second: B) -> Self {
+        Self { first, second }
+    }
+}
+
+impl<A, B> Verifier for VerifySequence<A, B>
+where
+    A: Verifier,
+    B: Verifier<Request = A::Request, Response = A::Response, Error = A::Error>,
+{
+    type Request = A::Request;
+    type Response = A::Response;
+    type Error = A::Error;
+
+    fn request(&mut self, request: &Self::Request) {
+        match self.first.has_finished() {
+            Ok(false) => self.first.request(request),
+            Ok(true) => self.second.request(request),
+            Err(_) => {}
+        }
+    }
+
+    fn response(&mut self, response: &Self::Response) {
+        match self.first.has_finished() {
+            Ok(false) => self.first.response(response),
+            Ok(true) => self.second.response(response),
+            Err(_) => {}
+        }
+    }
+
+    fn has_finished(&self) -> Result<bool, Self::Error> {
+        match self.first.has_finished() {
+            Ok(true) => self.second.has_finished(),
+            result => result,
+        }
+    }
+}
