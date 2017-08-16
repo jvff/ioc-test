36e2cd09ce08ad5c51e819f74e44525934f85d63
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-16 21:46:44 +0000

    Remove mock_service::When

--- src/mock_service/mock_service_factory.rs
+++ src/mock_service/mock_service_factory.rs
@@ -7,7 +7,6 @@ use std::sync::{Arc, Mutex};
 use tokio_service::NewService;
 
 use super::mock_service::MockService;
-use super::when::When;
 
 macro_rules! request_response_map {
     ( $object:expr , $($request:expr => $response:expr),+ $(,)* ) => {
@@ -32,43 +31,6 @@ where
             expected_requests,
             requests_to_verify,
         }
-    }
-
-    pub fn when<C>(&mut self, request: C) -> When<A, B>
-    where
-        C: Into<A>,
-    {
-        When::new(self, request.into())
-    }
-
-    pub fn expect(&mut self, request: A, response: B) -> &mut Self {
-        {
-            let mut expected_requests = self.expected_requests.lock().expect(
-                "another thread panicked while holding a lock to the mock \
-                 request map",
-            );
-
-            expected_requests.insert(request, response);
-        }
-
-        self
-    }
-
-    pub fn verify<C>(&mut self, request: C) -> &mut Self
-    where
-        C: Into<A>,
-    {
-        {
-            let mut requests_to_verify =
-                self.requests_to_verify.lock().expect(
-                    "another thread panicked while holding a lock to the mock \
-                     request verification set",
-                );
-
-            requests_to_verify.insert(request.into());
-        }
-
-        self
     }
 }
 
--- src/mock_service/mod.rs
+++ src/mock_service/mod.rs
@@ -2,7 +2,6 @@ mod errors;
 mod handle_request;
 mod mock_service;
 
-mod when;
 #[macro_use]
 mod mock_service_factory;
 
@@ -10,4 +9,3 @@ pub use self::errors::{Error, ErrorKind, Result};
 pub use self::handle_request::HandleRequest;
 pub use self::mock_service::MockService;
 pub use self::mock_service_factory::MockServiceFactory;
-pub use self::when::When;
