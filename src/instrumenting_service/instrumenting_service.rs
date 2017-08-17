a6b8184a58da3d199a1bfe13ddd193119174ed15
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-17 19:06:12 +0000

    Implement FiniteService for InstrumentingService
    - Move has_finished method to trait implementation

--- src/instrumenting_service/instrumenting_service.rs
+++ src/instrumenting_service/instrumenting_service.rs
@@ -3,6 +3,7 @@ use std::sync::{Arc, Mutex};
 use tokio_service::Service;
 
 use super::instrumented_response::InstrumentedResponse;
+use super::super::async_server::FiniteService;
 use super::verifier::Verifier;
 
 pub struct InstrumentingService<T, V>
@@ -24,14 +25,6 @@ where
             service,
             verifier: Arc::new(Mutex::new(verifier)),
         }
-    }
-
-    pub fn has_finished(&self) -> Result<bool, V::Error> {
-        let verifier = self.verifier.lock().expect(
-            "another thread panicked while holding a lock to a verifier",
-        );
-
-        verifier.has_finished()
     }
 }
 
@@ -56,5 +49,20 @@ where
             self.service.call(request),
             self.verifier.clone(),
         )
+    }
+}
+
+impl<T, V> FiniteService for InstrumentingService<T, V>
+where
+    T: Service,
+    V: Verifier<Request = T::Request, Response = T::Response>,
+    T::Error: From<V::Error>,
+{
+    fn has_finished(&self) -> Result<bool, T::Error> {
+        let verifier = self.verifier.lock().expect(
+            "another thread panicked while holding a lock to a verifier",
+        );
+
+        Ok(verifier.has_finished()?)
     }
 }
