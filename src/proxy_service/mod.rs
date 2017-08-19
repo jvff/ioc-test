2826f66bd86d8c5a3a4ac9979eeab88549ffe828
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-19 20:56:08 +0000

    Create initial proxy_service::errors sub-module

--- src/proxy_service/mod.rs
+++ src/proxy_service/mod.rs
@@ -1,7 +1,9 @@
+mod errors;
 mod flush_request;
 mod proxy_service;
 mod proxy_request;
 mod send_request;
 mod wait_for_response;
 
+pub use self::errors::{Error, ErrorKind, Result};
 pub use self::proxy_service::ProxyService;
