3a463c850357c52fe6558998a75a17bb20c6d632
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-21 13:03:01 +0000

    Implement ClientProto for ScpiProtocol

--- src/scpi/protocol.rs
+++ src/scpi/protocol.rs
@@ -1,7 +1,8 @@
 use tokio_io::{AsyncRead, AsyncWrite};
 use tokio_io::codec::Framed;
-use tokio_proto::pipeline::ServerProto;
+use tokio_proto::pipeline::{ClientProto, ServerProto};
 
+use super::client_codec::ScpiClientCodec;
 use super::errors::{Error, Result};
 use super::requests::ScpiRequest;
 use super::response::ScpiResponse;
@@ -18,5 +19,20 @@ impl<T: AsyncRead + AsyncWrite + 'static> ServerProto<T> for ScpiProtocol {
 
     fn bind_transport(&self, io: T) -> Self::BindTransport {
         Ok(io.framed(ScpiServerCodec))
+    }
+}
+
+impl<T> ClientProto<T> for ScpiProtocol
+where
+    T: AsyncRead + AsyncWrite + 'static,
+{
+    type Request = ScpiRequest;
+    type Response = ScpiResponse;
+    type Error = Error;
+    type Transport = Framed<T, ScpiClientCodec>;
+    type BindTransport = Result<Self::Transport>;
+
+    fn bind_transport(&self, io: T) -> Self::BindTransport {
+        Ok(io.framed(ScpiClientCodec))
     }
 }
