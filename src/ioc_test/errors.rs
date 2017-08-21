e62da702c00e75888f9ee9bae8c0339666c21cfb
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-21 13:42:27 +0000

    Link ioc_test::Error with proxy_service::Error
    - Also rename ServiceError into MockServiceError to make it clearer and
      avoid ambiguity

--- src/ioc_test/errors.rs
+++ src/ioc_test/errors.rs
@@ -1,14 +1,15 @@
 use std::io;
 use std::net::AddrParseError;
 
-use super::super::{async_server, mock_service, ioc, scpi};
+use super::super::{async_server, mock_service, proxy_service, ioc, scpi};
 
 error_chain! {
     links {
         IocError(ioc::Error, ioc::ErrorKind);
+        MockServiceError(mock_service::Error, mock_service::ErrorKind);
+        ProxyServiceError(proxy_service::Error, proxy_service::ErrorKind);
         ScpiError(scpi::Error, scpi::ErrorKind);
         ServerError(async_server::Error, async_server::ErrorKind);
-        ServiceError(mock_service::Error, mock_service::ErrorKind);
     }
 
     foreign_links {
