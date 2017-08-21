2680112472e5b7e060a91eb65826269fd7f807a6
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-21 15:10:15 +0000

    Link proxy_service::Error to std::io::Error

--- src/proxy_service/errors.rs
+++ src/proxy_service/errors.rs
@@ -1,6 +1,12 @@
+use std::io;
+
 use super::super::instrumenting_service::verifiers;
 
 error_chain! {
+    foreign_links {
+        IoError(io::Error);
+    }
+
     links {
         VerifierError(verifiers::Error, verifiers::ErrorKind);
     }
