898f317b4841b47ed583a26aa2559ff4a9871f10
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-22 18:37:37 +0000

    Use generic IocTestParameters in tests! macro

--- src/ioc_test/macros.rs
+++ src/ioc_test/macros.rs
@@ -4,11 +4,16 @@ macro_rules! tests {
         type Protocol = $protocol:ty;
         $( $test:ident ($name:expr) $body:tt )*
     ) => {
-        pub fn add_tests<S>(scheduler: &mut TestScheduler<S>)
+        pub fn add_tests<S, P>(scheduler: &mut TestScheduler<S>)
         where
-            S: TestSpawner<
-                TestSetup = IocTestSetup<MockTestParameters<$protocol>>
+            P: IocTestParameters<
+                Protocol = $protocol,
+                Request = <$protocol as ServerProto<TcpStream>>::Request,
+                Response = <$protocol as ServerProto<TcpStream>>::Response,
+                ProtocolError = <$protocol as ServerProto<TcpStream>>::Error,
             >,
+            P::ServiceError: From<<$protocol as ServerProto<TcpStream>>::Error>,
+            S: TestSpawner<TestSetup = IocTestSetup<P>>,
         {
             $(scheduler.add(|mut $test| {
                 $test.name($name);
