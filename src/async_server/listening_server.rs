9cfc48006614982600aae15d7d20c3510ddaf8ba
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-25 18:49:36 +0000

    Don't wait for service in ListeningServer
    - It's immediately available, no need to treat it as a future

--- src/async_server/listening_server.rs
+++ src/async_server/listening_server.rs
@@ -1,16 +1,16 @@
 use std::fmt::Display;
 use std::hash::Hash;
+use std::{io, mem};
 use std::sync::{Arc, Mutex};
 
 use futures::{Async, Future, Poll};
-use futures::future::{FutureResult, Join};
 use tokio_core::net::{TcpListener, TcpStream};
 use tokio_proto::pipeline::ServerProto;
 use tokio_service::NewService;
 
 use super::active_server::ActiveServer;
 use super::bound_connection_future::BoundConnectionFuture;
-use super::errors::{Error, NormalizeError};
+use super::errors::Error;
 use super::finite_service::FiniteService;
 
 pub struct ListeningServer<P, S>
@@ -19,8 +19,8 @@ where
     P::Error: Into<Error>,
     S: FiniteService<Request = P::Request, Response = P::Response>,
 {
-    connection_and_service:
-        Join<BoundConnectionFuture<P>, FutureResult<S, Error>>,
+    connection: BoundConnectionFuture<P>,
+    service: io::Result<S>,
 }
 
 impl<P, S> ListeningServer<P, S>
@@ -44,11 +44,9 @@ where
             Instance = S,
         >,
     {
-        let service = service_factory.new_service();
-        let connection = BoundConnectionFuture::from(listener, protocol);
-
         Self {
-            connection_and_service: connection.join(service.normalize_error()),
+            service: service_factory.new_service(),
+            connection: BoundConnectionFuture::from(listener, protocol),
         }
     }
 }
@@ -66,9 +64,15 @@ where
     type Error = Error;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        let (connection, service) =
-            try_ready!(self.connection_and_service.poll());
+        let connection = try_ready!(self.connection.poll());
+        let service = mem::replace(
+            &mut self.service,
+            Err(io::Error::new(
+                io::ErrorKind::Other,
+                "server listening state can't be polled for two connections",
+            )),
+        );
 
-        Ok(Async::Ready(ActiveServer::new(connection, service)))
+        Ok(Async::Ready(ActiveServer::new(connection, service?)))
     }
 }
