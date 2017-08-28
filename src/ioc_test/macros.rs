f924d31debdb1660194a5ac516944d70f00d8306
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-28 20:05:42 +0000

    Import dependencies in test! macro
    - Reduce number of imports user must specify

--- src/ioc_test/macros.rs
+++ src/ioc_test/macros.rs
@@ -4,22 +4,37 @@ macro_rules! tests {
         type Protocol = $protocol:ty;
         $( $test:ident ($name:expr) $body:tt )*
     ) => {
-        pub fn add_tests<S, P>(scheduler: &mut TestScheduler<S>)
-        where
-            P: IocTestParameters<
-                Protocol = $protocol,
-                Request = <$protocol as ServerProto<TcpStream>>::Request,
-                Response = <$protocol as ServerProto<TcpStream>>::Response,
-                ProtocolError = <$protocol as ServerProto<TcpStream>>::Error,
-            >,
-            P::ServiceError: From<<$protocol as ServerProto<TcpStream>>::Error>,
-            S: TestSpawner<TestSetup = IocTestSetup<P>>,
-        {
-            $(scheduler.add(|mut $test| {
-                $test.name($name);
-                $body
-            });)*
+        mod tests {
+            use tokio_core::net::TcpStream;
+            use tokio_proto::pipeline::ServerProto;
+
+            use ioc_test::{IocTestParameters, IocTestSetup, TestScheduler,
+                           TestSpawner};
+
+            use super::*;
+
+            pub fn add_tests<S, P>(scheduler: &mut TestScheduler<S>)
+            where
+                P: IocTestParameters<
+                    Protocol = $protocol,
+                    Request = <$protocol as ServerProto<TcpStream>>::Request,
+                    Response = <$protocol as ServerProto<TcpStream>>::Response,
+                    ProtocolError =
+                        <$protocol as ServerProto<TcpStream>>::Error,
+                >,
+                P::ServiceError: From<
+                    <$protocol as ServerProto<TcpStream>>::Error
+                >,
+                S: TestSpawner<TestSetup = IocTestSetup<P>>,
+            {
+                $(scheduler.add(|mut $test| {
+                    $test.name($name);
+                    $body
+                });)*
+            }
         }
+
+        pub use self::tests::add_tests;
     }
 }
 
