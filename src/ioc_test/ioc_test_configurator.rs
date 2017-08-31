e76eb8bf4437114466b85a05af1d4f2a76330d94
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-31 12:02:49 +0000

    Implement IocTestConfigurator for Fn(&mut test)

--- src/ioc_test/ioc_test_configurator.rs
+++ src/ioc_test/ioc_test_configurator.rs
@@ -7,3 +7,13 @@ where
 {
     fn configure(&mut self, test: &mut IocTestSetup<P>);
 }
+
+impl<F, P> IocTestConfigurator<P> for F
+where
+    F: Fn(&mut IocTestSetup<P>),
+    P: IocTestParameters,
+{
+    fn configure(&mut self, test: &mut IocTestSetup<P>) {
+        self(test)
+    }
+}
